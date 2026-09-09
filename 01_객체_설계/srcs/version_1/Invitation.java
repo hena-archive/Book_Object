93a8143f312ce67229a84605909117233fa61efc
hena <hena.journey@gmail.com>
2026-09-09 15:12:01 +0900

    Comment: 주석 추가

--- 01_객체_설계/srcs/version_1/Invitation.java
+++ 01_객체_설계/srcs/version_1/Invitation.java
@@ -1,5 +1,11 @@
 import java.time.LocalDateTime;
 
 public class Invitation {
+    // LocalDateTime = LocalDate + LocalTime
     private LocalDateTime when;
 }
+
+// LocalDate : 날짜 (year-month-day)
+// LocalTime : 시간 (hour-minute-second)
+// LocalDateTime : 날짜와 시간
+// ZonedDateTime : 시간대가 있는 날짜와 시간
