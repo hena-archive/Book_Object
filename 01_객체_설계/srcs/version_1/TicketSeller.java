0590ce9924db901f2b0001c182774a790ab3e05a
hena <hena.journey@gmail.com>
2026-09-09 15:52:07 +0900

    Comment: 생성자 및 메서드 분리 주석 추가

--- 01_객체_설계/srcs/version_1/TicketSeller.java
+++ 01_객체_설계/srcs/version_1/TicketSeller.java
@@ -1,10 +1,12 @@
 public class TicketSeller {
     private TicketOffice ticketOffice;
 
+// == 생성자(Constructor) ==============
     public TicketSeller(TicketOffice ticketOffice) {
         this.ticketOffice = ticketOffice;
     }
 
+// == 메서드(Method) ==============
     public TicketOffice getTicketOffice() {
         return ticketOffice;
     }
