309809068cc6da4a109162c284c62b0ca4e21bcc
hena <hena.journey@gmail.com>
2026-09-09 15:50:58 +0900

    Comment: asList 및 ..., addAll 설명 추가

--- 01_객체_설계/srcs/version_1/TicketOffice.java
+++ 01_객체_설계/srcs/version_1/TicketOffice.java
@@ -6,11 +6,18 @@ public class TicketOffice {
     private Long amount;
     private List<Ticket> tickets = new ArrayList<>();
 
+// == 생성자(Constructor) ==============
     public TicketOffice(Long amount, Ticket ... tickets) {
         this.amount = amount;
+
+        // Arrays.asList()는 배열을 List 형태로 변환해주는 메서드
         this.tickets.addAll(Arrays.asList(tickets));
+        // for (Ticket ticket : tickets) {
+        //     this.tickets.add(ticket);
+        // }
     }
 
+// == 메서드(Method) ==============
     public Ticket getTicket() {
         return tickets.remove(0);
     }
